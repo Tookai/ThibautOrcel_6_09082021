1260748188029d2c07e136b90fb547d763beb03c
Thibaut <tybo.orcel@gmail.com>
2021-08-09 16:54:27 +0200

    connecting to mongoDB

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 // ----------
 const cors = require("cors");
 const helmet = require("helmet");
+const mongoose = require("mongoose");
 // ----------
 
 // ----------
@@ -12,12 +13,19 @@ app.use(cors());
 // Headers Protection
 app.use(helmet());
 // ----------
-
+mongoose
+  .connect("mongodb+srv://Tookai:Thibaut21@cluster0.oniut.mongodb.net/projet6?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+// ----------
 app.use((req, res) => {
   res.json({ message: "salut toi" });
 });
 
 // ----------
 app.listen(3000, () => {
-  console.log("backend is running");
+  console.log("La fin du dos est en train de courir");
 });
