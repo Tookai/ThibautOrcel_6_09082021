89640dc0ddb07dee0e666faa664b6917d8b65ba0
Thibaut <tybo.orcel@gmail.com>
2021-08-09 16:35:12 +0200

    start express server

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 // Hello World !
+const express = require("express");
+const app = express();
+
+app.use((req, res) => {
+  res.json({ message: "salut toi" });
+});
+
+app.listen(3000, () => {
+  console.log("backend is running");
+});
