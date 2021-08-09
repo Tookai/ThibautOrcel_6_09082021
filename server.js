27b1524326e7002f81612c274ee9f95bc1f5eb99
Thibaut <tybo.orcel@gmail.com>
2021-08-09 16:41:04 +0200

    prevent cors error

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
-// Hello World !
+// ----------
 const express = require("express");
 const app = express();
+// ----------
+const cors = require("cors");
+const helmet = require("helmet");
+// ----------
+
+// ----------
+// Cross Origin Resource Sharing
+app.use(cors());
+// Headers Protection
+app.use(helmet());
+// ----------
 
 app.use((req, res) => {
   res.json({ message: "salut toi" });
 });
 
+// ----------
 app.listen(3000, () => {
   console.log("backend is running");
 });
