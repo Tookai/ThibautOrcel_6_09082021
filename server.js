c1abfcac4c5e464e7563e5dd71c762cf2f271592
Thibaut <tybo.orcel@gmail.com>
2021-08-26 16:06:39 +0200

    added route error 404

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose
   })
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
-// ---------- 
+// ----------
 // Cross Origin Resource Sharing
 app.use(cors());
 // Headers Protection
@@ -34,6 +34,10 @@ app.use("/images", express.static(path.join(__dirname, "images")));
 // ---------- ROUTES
 app.use("/api/auth", userRoute);
 app.use("/api/sauces", sauceRoute);
+// --- 404:Not Found Error
+app.all("*", (req, res) => {
+  res.status(404).json(`Can't find ${req.originalUrl} on this server!`);
+});
 // ----------
 app.listen(3000, () => {
   console.log("La fin du dos est en train de courir");
